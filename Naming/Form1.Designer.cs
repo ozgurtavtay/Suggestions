92705ba8431b87a7358193dfcf8968361f639136
ÖZGÜR TAVTAY <l5309036@std.yildiz.edu.tr>
2019-02-05 21:45:12 +0300

    Add project files.

---
+++ Naming/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Naming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSuggestions = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start typing a name in the input field below:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Suggestions:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First name:";
+            // 
+            // lblSuggestions
+            // 
+            this.lblSuggestions.AutoSize = true;
+            this.lblSuggestions.Location = new System.Drawing.Point(86, 54);
+            this.lblSuggestions.Name = "lblSuggestions";
+            this.lblSuggestions.Size = new System.Drawing.Size(0, 13);
+            this.lblSuggestions.TabIndex = 3;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(76, 96);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 4;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBoxName_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 129);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.lblSuggestions);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Name Suggestions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSuggestions;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Naming/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Naming
+{
+    public partial class Form1 : Form
+    {
+        Suggestions s = new Suggestions();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxName_TextChanged(object sender, EventArgs e)
+        {
+            string str = textBoxName.Text;
+            string hint = "";
+
+            if (str != "")
+            {
+                bool isFirst = true;
+                foreach (string name in s.Names)
+                {
+                    if (name.ToLower().IndexOf(str) == 0)
+                    {
+                        if (isFirst)
+                        {
+                            hint += name;
+                            isFirst = false;
+                        }
+                        else
+                        {
+                            hint += " --- " + name;
+                        }
+                    }
+                }
+
+                if (hint == "")
+                {
+                    hint = "No Suggestions";
+                }
+
+            }
+
+            lblSuggestions.Text = hint;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                s.Names.Add(textBoxName.Text);
+                MessageBox.Show(textBoxName.Text + " Added to Suggestions List");
+                textBoxName.Clear();
+                textBoxName.Focus();
+                lblSuggestions.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
